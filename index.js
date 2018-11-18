b05e17075ffb6399cfe1b3bafb83b58977afb799
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-18 22:18:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten(name) {
-  return kittens.slice(0)
+  return kittens.slice(0, kittens.length-1)
 }
 function removeFirstKitten(name) {
   return kittens.slice(1)
